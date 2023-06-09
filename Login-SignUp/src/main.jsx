78bb1cc6e50e638a7448aa0a93078ee076a22087
Joshua Owusu <joshbillion1@gmail.com>
2023-06-09 05:18:46 +0000

    Commit auto_update

--- Login-SignUp/src/main.jsx
+++ Login-SignUp/src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import './index.css';
-// import login from "./components/Login/login.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
